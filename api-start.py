0d0acf532ad0f2da59b2fbe2047f99222b24ab2d
Phillip Kent <phillip.kent@xmlsoup.com>
2017-05-05 14:23:55 +0100

    api-start.py: Add 'createBucket' XML strings

--- api-start.py
+++ api-start.py
@@ -15,6 +15,13 @@ corsMD5hash = hashlib.md5()
 corsMD5hash.update(corsConfig)
 corsMD5encoded = base64.b64encode(corsMD5hash.digest())
 
+# XML data strings for new bucket in different regions (default region EU is selected when LocationConstraint is not specified)
+createBucketCH = '<CreateBucketConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/"><LocationConstraint>ch</LocationConstraint></CreateBucketConfiguration>'
+createBucketDE = '<CreateBucketConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/"><LocationConstraint>de</LocationConstraint></CreateBucketConfiguration>'
+
+# Create a bucket in the DE region
+## requests.put('http://my-de-bucket.s3-de.object.vdc.interoute.com', auth=OSauth, data=createBucketDE)
+
 # PUT the CORS configuration to an existing bucket (replace 'mybucket' with an actual bucket name)
 ## requests.put('http://mybucket.s3-eu.object.vdc.interoute.com/?cors', auth=OSauth, data=corsConfig, headers={'content-md5':corsMD5encoded})
 
@@ -24,6 +31,13 @@ corsMD5encoded = base64.b64encode(corsMD5hash.digest())
 
 # GET contents of an object (replace 'mybucket' with an actual bucket name)
 ## r1 = requests.get('http://mybucket.s3-eu.object.vdc.interoute.com/testobject.txt', auth=OSauth)
+
+# XML data strings for enabling or suspending versioning status of a bucket (use with a '?versioning' request)
+versioningEnabled = '<VersioningConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/"><Status>Enabled</Status></VersioningConfiguration>'
+versioningSuspended = '<VersioningConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/"><Status>Suspended</Status></VersioningConfiguration>'
+
+# Example of Versioning enable (the bucket must already exist)
+## requests.put('http://testversioning.s3-eu.object.vdc.interoute.com/?versioning', auth=OSauth, data=versioningEnabled)
 
 
 
