b59c6f7994346ba121c96151b64b8e9ca5eec961
Phillip Kent <phillip.kent@xmlsoup.com>
2017-08-17 13:02:18 +0100

    s3ls.py: first and incomplete release (only checks ACL permissions for buckets)

--- s3ls.py
+++ s3ls.py
@@ -16,22 +16,11 @@
 #   https://docs.python.org/2/library/xml.etree.elementtree.html
 #   https://stackoverflow.com/questions/34009992/python-elementtree-default-namespace
 #   https://stackoverflow.com/questions/29785974/determine-s3-bucket-region
+#   https://stackoverflow.com/questions/8356501/python-format-tabular-output
+#   https://stackoverflow.com/questions/9535954/printing-lists-as-tabular-data
+#   https://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
 # https://stackoverflow.com/questions/1915564/python-convert-a-dictionary-to-a-sorted-list-by-value-instead-of-key
 # https://duckduckgo.com/?q=python+itemgetter&t=lm&ia=qa
-
-'''
-from __future__ import print_function
-from collections import OrderedDict
-from copy import deepcopy
-import base64
-import pprint
-import time
-import datetime
-import dateutil.parser
-import pytz
-import textwrap
-import re
-'''
 
 import ConfigParser
 import argparse
@@ -42,6 +31,13 @@ import sys
 import json
 import xml.dom.minidom
 import xml.etree.ElementTree as ET
+from math import log
+
+# Function pretty_size to pretty print object size values
+# Reference: https://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
+def pretty_size(n,pow=0,b=1024,u='',pre=['']+[p for p in 'KMGTP']):
+    pow,n=min(int(log(max(n*b**pow,1),b)),len(pre)-1),n*b**pow
+    return "%%.%if%%s%%s"%abs(pow%(-pow-1))%(n/b**float(pow),pre[pow],u)
 
 # Tabular print function
 # Reference: https://stackoverflow.com/questions/8356501/python-format-tabular-output
@@ -60,10 +56,6 @@ def print_table_with_state(table_with_state):
     for line in table_with_state:
         print line[numcols] + "" + "  ".join("{:{}}".format(x, col_width[i])
                                 for i, x in enumerate(line[:numcols])) + "" + line[numcols+1]
-
-# function to prettyprint XML
-def xmlp(xmlString):
-   print xml.dom.minidom.parseString(xmlString).toprettyxml()
 
 # STEP: Load the Object Storage data file (information about regions, storage policies etc)
 json_file = 'interoute-object-storage-refdata.json'
@@ -90,17 +82,23 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--config", default=os.path.join(os.path.expanduser('~'), '.s3cfg'),
                     help="path/name of the config file to be used for the S3 URL and S3 keys (default is ~/.s3cfg)")
 parser.add_argument("-r", "--region", default='', help="display for the specified Object Storage region (default is all regions)")
-parser.add_argument("-s", "--storagepolicies", action='store_true', help="display the storage policy information for each bucket")
-parser.add_argument("-p", "--permissions", action='store_true', help="display the ACL permissions for each bucket/folder/object")
+parser.add_argument("-b", "--bucketsonly", action='store_true', help="display only bucket information (without content)")
+parser.add_argument("-s", "--storagepolicy", action='store_true', help="display the storage policy information")
+parser.add_argument("-p", "--permissions", action='store_true', help="display the ACL permissions for each bucket")
+parser.add_argument("-o", "--colored", action='store_true', help="use colorised output (green indicates ACL public permission setting for the bucket - object permission is NOT checked)")
+parser.add_argument("-H", "--humanreadable", action='store_true', help="show human-readable size values for objects (powers of 1024)")
 ## ** ADD OPTION FOR FILTERING ONLY 'PUBLIC' PERMISSIONS WHICH CAN BE OF SEVERAL TYPES (ACL, static website, time-limited share, ?)
 ## ** ADD OPTION FOR SETTING AN INITIAL BUCKET (OR BUCKET/FOLDER) -- DEFAULT 'NON-RECURSIVE' TO SHOW CONTENTS AT TOP LEVEL OF THE BUCKET/FOLDER
 ## ** ADD OPTION RECURSIVE (INCLUDE ALL LEVELS BELOW INITIAL BUCKET/FOLDER LEVEL) OR NON-RECURSIVE (LIST ONLY OBJECTS/FOLDERS AT THE INITIAL LEVEL)
-## ** ADD OPTION FOR FLAT DISPLAY OF FOLDER1/FOLDER2/.../OBJECT OR DEFAULT HIERARCHICAL-INDENTED OUTPUT
+## ** ADD OPTION FOR 'FLAT' DISPLAY OF FULL PATH "BUCKETNAME/FOLDER1/FOLDER2/.../OBJECT" (DEFAULT) OR HIERARCHICAL-INDENTED OUTPUT
 ## ** ADD OPTIONS FOR SHOWING VERSIONING INFORMATION??
-## ** ADD OPTION FOR COLOURISED OUTPUT (OF PUBLIC PERMISSION STATUS)
-coloredOutput = True
 config_file = parser.parse_args().config
 region_selected = parser.parse_args().region.upper()
+bucketsOnlyDisplay = parser.parse_args().bucketsonly
+coloredOutput = parser.parse_args().colored
+storagePolicyDisplay = parser.parse_args().storagepolicy
+permissionsDisplay = parser.parse_args().permissions
+humanreadableSizeDisplay= parser.parse_args().humanreadable
 
 if region_selected:
    if region_selected not in regions:
@@ -136,21 +134,22 @@ resp = requests.get('https://' + OSdata[regions[0]]['apiEndpoint'], auth=OSauth)
 ##xmlp(resp.text)
 allBuckets = ET.fromstring(resp.text)
 
-# Set up Dict for bucket data, with keys = the bucket names
+# Set up Dict for bucket data, with keys=the bucket names
 bucketsDataDict = {}
 for b in allBuckets.find("s3:Buckets", ns).findall('s3:Bucket', ns):
    bname = b.find('s3:Name', ns).text
    bucketsDataDict[bname] = {}
    bucketsDataDict[bname]['name'] = bname
    bucketsDataDict[bname]['created'] = b.find('s3:CreationDate', ns).text.split('.')[0]
-   #FOLLOWING CALL DOES NOT WORK BECAUSE YOU NEED TO ALREADY KNOW THE REGION FOR THE API CALL TO SUCCEED!
-   #  requests.get('https://' + bname + "." + OSdata[regions[0]]['apiEndpoint'], auth=OSauth).headers['x-amz-bucket-region'].upper()
    locationResp = ET.fromstring(requests.get('https://' + bname + "." + OSdata[regions[0]]['apiEndpoint'] + '/?location', auth=OSauth).text).text
    # Default region has blank value for '?location'
    if not(locationResp): 
       bucketsDataDict[bname]['region'] = regions[0]
    else:
       bucketsDataDict[bname]['region'] = locationResp.upper()
+   if storagePolicyDisplay:
+      bucketsDataDict[bname]['storagepolicyid'] = requests.get('https://' + bname + "." + OSdata[bucketsDataDict[bname]['region']]['apiEndpoint'], auth=OSauth).headers['x-gmt-policyid']   
+      bucketsDataDict[bname]['storagepolicyname'] = [sp['storagePolicyInternalName'] for sp in OSdata[bucketsDataDict[bname]['region']]['storagePolicies'] if sp['storagePolicyId']==bucketsDataDict[bname]['storagepolicyid']][0]
 
 # Get ACL permissions data for buckets (if region_selected option is input, only do this for buckets in the one specified region)
 for bname in bucketsDataDict.keys():
@@ -176,13 +175,21 @@ for bname in bucketsDataDict.keys():
       # if any of the grants permit public access then set 'publicFlag' to True
       bucketsDataDict[bname]['publicFlag'] = any(publicTest)
 
-      
-# ** TEST OUTPUT **
-# ** TO ADD: SORTING OF BUCKET OUTPUT BY: CREATE TIME, REGION, NAME (ASCENDING/DESCENDING IN EACH CASE)
-# ** TO ADD: SORTING OF FOLDER/OBJECT OUTPUT BY: CREATED TIME, NAME (ASCENDING/DESCENDING IN EACH CASE)
+#Build the content of the table header depending on the display options
+headerRow = ["DATE", "REGION"]
+if storagePolicyDisplay:
+   headerRow += ["POLICY"]
+## TO DO: "SIZE" should be omitted for option 'bucketsOnlyDisplay'
+if not(bucketsOnlyDisplay):
+   headerRow += ["SIZE"]
+headerRow += ["NAME"]
+if permissionsDisplay:
+   headerRow += ["PERMISSIONS"]
+headerRow += ["", ""]
 
-outputTable = [["CREATED_DATE", "REGION", "NAME", "PERMISSIONS", "", ""]]
-for bname in bucketsDataDict.keys():
+outputTable = []
+
+for bname in bucketsDataDict.keys():         
    # check if showing content for all regions, or this bucket is in the specified region
    if not(region_selected) or (region_selected and bucketsDataDict[bname]['region'] == region_selected):
       if coloredOutput and bucketsDataDict[bname]['publicFlag']:
@@ -191,9 +198,58 @@ for bname in bucketsDataDict.keys():
       else:
          stateColorOn = ""
          stateColorOff = ""
-      permissions = map(lambda x: "%s (%s)" % (x['name'], x['permission']), bucketsDataDict[bname]['accessACL'])
-      outputTable += [[bucketsDataDict[bname]['created'], bucketsDataDict[bname]['region'], bucketsDataDict[bname]['name'], permissions, stateColorOn, stateColorOff]]
+      #Create output row for the bucket
+      outputRow = [bucketsDataDict[bname]['created'], bucketsDataDict[bname]['region']]
+      if storagePolicyDisplay:
+         outputRow += [bucketsDataDict[bname]['storagepolicyname']]
+      if not(bucketsOnlyDisplay):
+         outputRow += [""]  # print blank as SIZE for a bucket - not displayed for 'bucketsOnlyDisplay'
+      outputRow += [bucketsDataDict[bname]['name']]
+      if permissionsDisplay:
+         permissions = map(lambda x: "%s (%s)" % (x['name'], x['permission']), bucketsDataDict[bname]['accessACL'])
+         permissionsString = ", ".join(permissions)
+         outputRow += [permissionsString]
+      outputRow += [stateColorOn, stateColorOff]
+      outputTable += [outputRow]
+      #Create output rows for the bucket contents
+      if not(bucketsOnlyDisplay):
+         bucketContents=ET.fromstring(requests.get('https://' + bname + "." + OSdata[bucketsDataDict[bname]['region']]['apiEndpoint'], auth=OSauth).text)
+         for c in bucketContents.findall("s3:Contents", ns):
+            # Key = the folder/object path and name
+            cKey = c.find("s3:Key", ns).text
+            thisIsFolder =  (cKey[-1] == '/')
+            outputRow = [c.find("s3:LastModified", ns).text.split('.')[0], bucketsDataDict[bname]['region']] 
+            if storagePolicyDisplay:
+               outputRow += [bucketsDataDict[bname]['storagepolicyname']]
+            objectSize = int(c.find("s3:Size", ns).text)
+            if thisIsFolder:
+               # for a folder, print 'f' for SIZE
+               outputRow += ["f"]
+            elif humanreadableSizeDisplay:
+               outputRow += [pretty_size(objectSize)]
+            else: 
+               outputRow += [str(objectSize)]
+            outputRow += [ "  "+bucketsDataDict[bname]['name'] + "/" + cKey] 
+            if permissionsDisplay:
+               outputRow += [""]
+            outputRow += [stateColorOn, stateColorOff]
+            outputTable += [outputRow]
+      
 
-print_table_with_state(outputTable)
+# ** TO ADD: OPTIONAL SORTING OF BUCKET OUTPUT BY: DATE, REGION, NAME (ASCENDING/DESCENDING IN EACH CASE)
+# ** TO ADD: SORTING OF OBJECT/FOLDER OUTPUT BY: DATE, SIZE, NAME (ASCENDING/DESCENDING IN EACH CASE)
+# Default sort: primary=region ascending, secondary=name ascending 
+if bucketsOnlyDisplay:
+   if storagePolicyDisplay:
+      outputTable.sort(key=lambda x: (x[1], x[3].strip()))
+   else:
+      outputTable.sort(key=lambda x: (x[1], x[2].strip()))
+else:
+   if storagePolicyDisplay:
+      outputTable.sort(key=lambda x: (x[1], x[4].strip()))
+   else:
+      outputTable.sort(key=lambda x: (x[1], x[3].strip()))
+
+print_table_with_state([headerRow] + outputTable)
 
       
